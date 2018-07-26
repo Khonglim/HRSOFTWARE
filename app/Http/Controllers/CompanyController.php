7fbe6a0b2a8cb4befe61b3ee42873be0fd6d3e36
Panya Thongmuang <35572439+panyawutt@users.noreply.github.com>
2018-07-26 13:50:56 +0700

    Revert "wut"
    This reverts commit 65f24c6af8973aadad75e48c1849d6739495f271.

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -31,7 +31,7 @@ class CompanyController extends Controller
      */
     public function create()
     {
-        return view('fromcreatcompany');
+        return view('formompany');
     }
 
     /**
