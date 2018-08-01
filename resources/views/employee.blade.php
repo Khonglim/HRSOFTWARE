e6f15a1e49b95b873669b0c0516f09ace137c1ef
นราธร ข้องหลิม <33591517+Khonglim@users.noreply.github.com>
2018-08-02 01:00:13 +0700

    jame

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -18,13 +18,50 @@
                          <th>ชื่อ</th>
                          <th>นามสกุล</th>
                          <th>ชื่อเล่น</th>
-                         <th>วันเกิด</th>
-                         <th>เพศ</th>
-                         <th>อีเมล</th>
-                         <th>เพิ่มเติ่ม</th>
                       </tr>
                     </thead>
-                  
+                    <tbody>
+                      @forelse($personal as $l)
+                     
+                     <tr>
+                   <td> {{$l['name']}} </td>
+                   <td> {{$l['lastname']}} </td>
+                   <td> {{$l['nikname']}} </td>
+                   <td>  
+                        <a href="{{'employee/'.$l['id'].'/edit'}}" class="btn btn-primary btn-xs" ><i class="fa fa-pencil"></i></a>
+                        <button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myModal{{$l['id']}}"><i class=" fa fa-trash"></i></button>
+          
+         
+              <div id="myModal{{$l['id']}}" class="modal fade" role="dialog">
+                        <div class="modal-dialog">
+                           <div class="modal-content">
+                       <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+      <h4 class="modal-title">ลบข้อมูล</h4>
+      </div>
+      <div class="modal-body">
+      {{ Form::open(['route' => ['employee.destroy',$l['id'], 'method' => "DELETE"] ]) }}
+      <input type="hidden" name="_method" value="delete" / >
+      <p>คุณต้องการลบ{{$l['company_Name']}}ใช่หรือไม่!!!!</p>  
+      </div>
+      <div class="modal-footer">
+      <button type="summit" class="btn btn-danger">ลบ</button>
+      <button type="button" class="btn btn-default" data-dismiss="modal">ยกเลิก</button>
+      </div>
+      {{ Form::close() }}  
+      </div>
+      
+      </div>
+      </div>
+      </td>
+      </tr>
+   
+       @empty
+      <tr>
+      <td colspan="6" > No data !!</td>
+      </tr>
+        @endforelse
+                    </tbody>
               </table> 
                </div>
               <footer class="panel-footer">
