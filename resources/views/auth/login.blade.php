78641013340082f04d6a7fb3e5602641a05b1d01
นราธร ข้องหลิม <33591517+Khonglim@users.noreply.github.com>
2018-08-06 01:22:56 +0700

    jame

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,52 @@
+<!DOCTYPE html>
+<html>
 <head>
-    <link rel="stylesheet" href="css/bootstrap.min.css" >
-    <!-- //bootstrap-css -->
-    <!-- Custom CSS -->
-    <link href="css/style.css" rel='stylesheet' type='text/css' />
-    <link href="css/style-responsive.css" rel="stylesheet"/>
-    <!-- font CSS -->
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <!-- font-awesome icons -->
-    <link rel="stylesheet" href="css/font.css" type="text/css"/>
-    <link href="css/font-awesome.css" rel="stylesheet">
-    <!-- //font-awesome icons -->
-    <script src="js/jquery2.0.3.min.js"></script>
-    </head>
-    <body>
-    <div class="log-w3">
-    <div class="w3layouts-main">
-        <h2>Sign In Now</h2>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="/plugins/iCheck/square/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a ><b>NGG</b>LTE</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sign in to start your session</p>
           <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
             @csrf
-          <input id="identity" type="identity" class="ggg" name="identity"  placeholder="USERNAME"
-    value="{{ old('identity') }}" autofocus>
+            <div class="form-group has-feedback">
+          <input id="identity" type="identity" class="form-control" name="identity"  placeholder="USERNAME"
+    value="{{ old('identity') }}" autofocus/>
+</div>
     @if ($errors->has('identity'))
     <span class="help-block"><strong>{{ $errors->first('identity') }}</strong></span>
     @endif
-    <input id="password" type="password" class="ggg {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required   placeholder="PASSWORD" >
-    
+    <div class="form-group has-feedback">
+    <input id="password" type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required   placeholder="PASSWORD" />
+</div>
     @if ($errors->has('password'))
         <span class="invalid-feedback" role="alert">
             <strong>{{ $errors->first('password') }}</strong>
@@ -37,16 +59,26 @@
                     <div class="clearfix"></div>
                     <input type="submit" value="Sign In" name="login">
             </form>
-            <p>Don't Have an Account ? <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></p>
-    </div>
-    </div>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="js/scripts.js"></script>
-    <script src="js/jquery.slimscroll.js"></script>
-    <script src="js/jquery.nicescroll.js"></script>
-    <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
-    <script src="js/jquery.scrollTo.js"></script>
-    </body>
-    </html>
-    
+          
+              
+                </div>
+                <!-- /.login-box-body -->
+              </div>
+              <!-- /.login-box -->
+              
+              <!-- jQuery 3 -->
+              <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+              <!-- Bootstrap 3.3.7 -->
+              <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+              <!-- iCheck -->
+              <script src="/plugins/iCheck/icheck.min.js"></script>
+              <script>
+                $(function () {
+                  $('input').iCheck({
+                    checkboxClass: 'icheckbox_square-blue',
+                    radioClass: 'iradio_square-blue',
+                    increaseArea: '20%' /* optional */
+                  });
+                });
+              </script>
+              </body>
