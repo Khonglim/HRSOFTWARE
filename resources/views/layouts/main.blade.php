1270c30d697d714e7a92d4fd8096004c5e3bc525
นราธร ข้องหลิม <33591517+Khonglim@users.noreply.github.com>
2018-08-07 22:57:14 +0700

    jame

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,7 +23,7 @@
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
         
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition skin-blue fixed sidebar-mini">
    
     <div id="main">
             <div class="wrapper">
