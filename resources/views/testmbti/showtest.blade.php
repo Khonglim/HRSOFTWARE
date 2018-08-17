5de057f31bcc3e6e810c915b636da56d9379f618
Khonglim <33591517+Khonglim@users.noreply.github.com>
2018-08-18 00:08:40 +0700

    jame

--- resources/views/testmbti/showtest.blade.php
+++ resources/views/testmbti/showtest.blade.php
@@ -1,13 +1,13 @@
 <script src="http://code.jquery.com/jquery-latest.js"></script>
   <script type="text/javascript">
 	$(document).ready(function() {
-		$('#SelectionReset').click(function(){$(".uniqueSelection option").show();});
+		$('#SelectionReset').click(function(){$(".uniqueSelection1 option").show();});
 
-		$(".uniqueSelection").change(function(){
-			var indx = $(".uniqueSelection").index(this);
+		$(".uniqueSelection1").change(function(){
+			var indx = $(".uniqueSelection1").index(this);
 			var currentVal = $("option:selected",this).val();
 			
-			$(".uniqueSelection").each(function(index,val){
+			$(".uniqueSelection1").each(function(index,val){
 				if(indx != index){
 					$(this).find("option[value='"+currentVal+"']").hide();
 				}
@@ -16,19 +16,19 @@
 	});
   </script>
 
-One:<select name="listOne" id="listOne" class="uniqueSelection">
+One:<select name="listOne" id="listOne" class="uniqueSelection1">
 	<option value=''>กรุณาเลือก</option>
 	<option value='Steve Jobs'>Steve Jobs</option>
 	<option value='Roger Federuer'>Roger Federuer</option>
 	<option value='Tiger Woods'>Tiger Woods</option>
   </select><br />
-Second:<select name="listSecond" id="listSecond" class="uniqueSelection">
+Second:<select name="listSecond" id="listSecond" class="uniqueSelection1">
 	<option value=''>กรุณาเลือก</option>
 	<option value='Steve Jobs'>Steve Jobs</option>
 	<option value='Roger Federuer'>Roger Federuer</option>
 	<option value='Tiger Woods'>Tiger Woods</option>
   </select><br />
-Third:<select name="listThird" id="listThird" class="uniqueSelection">
+Third:<select name="listThird" id="listThird" class="uniqueSelection1">
 	<option value=''>กรุณาเลือก</option>
 	<option value='Steve Jobs'>Steve Jobs</option>
 	<option value='Roger Federuer'>Roger Federuer</option>
