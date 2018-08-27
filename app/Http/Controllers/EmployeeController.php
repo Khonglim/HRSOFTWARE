76462a00b0ce54bb511debe34970139ef05a7bd1
Jaonaii <yutthinun7640@hotmai.com>
2018-08-27 09:59:28 +0700

    nine

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -47,7 +47,7 @@ class EmployeeController extends Controller
             $sub[$district->name] = $district->name;
         }
         
-        return view('employ/formcreateemployee', compact('items',$items,'sub', $district));
+        return view('formregister', compact('items',$items,'sub', $district));
     }
 
     /**
