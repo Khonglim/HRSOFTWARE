aae4c2611f8bef5ac4ebaf88455d91380e3e5bcf
Jaonaii <yutthinun7640@hotmai.com>
2018-08-30 15:12:28 +0700

    nine

---
+++ app/Ngg_level.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ngg_level extends Model
+{
+    protected $table = 'ngg_level';
+}
---
+++ app/Ngg_sector.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ngg_sector extends Model
+{
+    protected $table = 'ngg_sector';
+}
