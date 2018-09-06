3567340ba104c2060ea8580a47b0248e0029d7b8
Panya Thongmuang <35572439+panyawutt@users.noreply.github.com>
2018-09-06 11:19:58 +0700

    wut

--- resources/views/testeva/starttest.blade.php
+++ resources/views/testeva/starttest.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.main')
 @section('title','| แบบประเมิน')
 @section('content')
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+header("Pragma: no-cache"); // HTTP/1.0
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+?>
 <div class="content-wrapper">
 	<style type="text/css">
   table{
