d096b383d0b91a78343e71a03148a201dde93416
Panya Thongmuang <35572439+panyawutt@users.noreply.github.com>
2018-10-04 15:07:34 +0700

    wut

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
     @if(Session::has('flash_message'))
     <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message') !!}</em></div>
 	@endif
+
+	
   
 
 </div>
