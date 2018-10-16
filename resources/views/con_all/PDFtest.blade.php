8bd1107c291a6492ce500d55a17cf356f8afae12
jame <narathorn182539@gmail.com>
2018-10-16 16:55:49 +0700

    jj

--- resources/views/con_all/PDFtest.blade.php
+++ resources/views/con_all/PDFtest.blade.php
@@ -1,57 +1,22 @@
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style>
-        @font-face {
-            font-family: 'THSarabunNew';
-            font-style: normal;
-            font-weight: normal;
-            src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
-        }
-        @font-face {
-            font-family: 'THSarabunNew';
-            font-style: normal;
-            font-weight: bold;
-            src: url("{{ public_path('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
-        }
-        @font-face {
-            font-family: 'THSarabunNew';
-            font-style: italic;
-            font-weight: normal;
-            src: url("{{ public_path('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
-        }
-        @font-face {
-            font-family: 'THSarabunNew';
-            font-style: italic;
-            font-weight: bold;
-            src: url("{{ public_path('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
+        body {
+            font-family: 'examplefont', sans-serif;
+            font-size: 18px;
         }
 
-        body {
-            font-family: "THSarabunNew";
-            font-size: 20px;
-        }
-        table{
-          border-collapse: collapse;
+        table {
+            border-collapse: collapse;
             width: 100%;
         }
-        td,th{
-          border:1px solid;
-         
-           
+
+        td,
+        th {
+            border: 1px solid;
         }
 
-         body { width: 100%; height: 100%; }  
-
-
-.page-break {
-    page-break-after: always;
-}
-</style>
-
-
-
-
+    </style>
     
 </head>
 <body>
