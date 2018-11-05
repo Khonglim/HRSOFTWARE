3f9dc9bd47c3c073231a1da6b0a24a8b7295ada4
jame <narathorn182539@gmail.com>
2018-11-05 10:13:34 +0700

    Update resources/views/con_all/conAll.blade.php

--- resources/views/con_all/conAll.blade.php
+++ resources/views/con_all/conAll.blade.php
@@ -1,20 +1,16 @@
 @extends('layouts.main')
-
-
 @section('content')
   <div class="content-wrapper">
     <div class="content container">
-     
-            <div class="box"  >
-                    <div class="box-header">
-                      <h3 class="box-title">สรุปผล</h3>
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body">
-                      <table id="company" class="table table-bordered table-striped">
+       <div class="box"  >
+          <div class="box-header">
+              <h3 class="box-title">สรุปผล</h3>
+            </div>
+              <!-- /.box-header -->
+             <div class="box-body">
+                    <table id="company" class="table table-bordered table-striped">
                         <thead>
                         <tr>
-                         
                           <th>ชื่อ</th>
                           <th>นาสกุล</th>
                           <th>เพิ่มเติ่ม</th>
@@ -23,43 +19,35 @@
                         <tbody>
                                 @forelse($personal as $l)
                                 @if($l['enable']==1)
-                               <tr>
-                             <td> {{$l['name']}} </td>
-                             <td> {{$l['lastname']}} </td>
+                      <tr>
+                      <td> {{$l['name']}} </td>
+                      <td> {{$l['lastname']}} </td>
                             
-                             <td>  
-                                  <a href="{{'con_all/'.$l['id']}}" class="btn btn-primary btn-xs" ><i class="fa fa-search"></i></a>
-                                 
-                                	<a href="{{url('pdfcon_all/'.$l['id'])}}" class="btn btn-success btn-xs " ><i class="fa fa-download"></i></a>
+                      <td>  
+                             <a href="{{'con_all/'.$l['id']}}" class="btn btn-primary btn-xs" ><i class="fa fa-search"></i></a>
+                              <a href="{{url('pdfcon_all/'.$l['id'])}}" class="btn btn-success btn-xs " ><i class="fa fa-download"></i></a>
                                   
                  
                    </td>
                 </tr>
-                @endif
+                    @endif
                  @empty
                 <tr>
-                <td colspan="6" > No data !!</td>
+                        <td colspan="6" > No data !!</td>
                 </tr>
                   @endforelse
                         </tbody>
                         <tfoot>
                         <tr>
-                        
                           <th>ชื่อ</th>
                           <th>นามสกุล</th>
                           <th>เพิ่มเติ่ม</th>
                         </tr>
                         </tfoot>
-                      </table>
-                    </div>
+                    </table>
                   </div>
-
-
-
-
-
-
-</div>
+            </div>
+      </div>
   </div>
  
   @endsection
