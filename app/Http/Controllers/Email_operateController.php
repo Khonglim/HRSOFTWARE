86bab086dd189f1dffeb6203c7952bbcd67f8e63
jame <narathorn182539@gmail.com>
2018-11-15 09:28:15 +0700

    jame

--- app/Http/Controllers/Email_operateController.php
+++ app/Http/Controllers/Email_operateController.php
@@ -4,18 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Operate;
+use App\Mail\HRSystemEmail;
+use Illuminate\Support\Facades\Mail;
 class Email_operateController extends Controller
 {
     public function email_send(Request $request)
     {
         $id=$request->id;
+        
         $operater = Operate::all();
         foreach ( $operater as  $user){
           if($user->id== $id && $user->signa1_60_enable == 0){
             $operate = Operate::find($id);
             $operate->signa1_60_enable =1;
             $operate->save();
-          return "เสร็จ";
+            $objEmail = new \stdClass();
+            $objEmail->email1 = $request->email1;
+            $objEmail->link1 = $request->link1;
+            Mail::to($objEmail->email1)->send(new HRSystemEmail($objEmail));
+
+          return "สำเสร็จ";
 
         }
 
