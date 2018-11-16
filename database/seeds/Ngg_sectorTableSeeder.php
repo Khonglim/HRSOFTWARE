fe6a71fb59ea3b69b1ae43eaac92f7ff0ad8c5e1
jame <narathorn182539@gmail.com>
2018-11-16 17:05:03 +0700

    Update Ngg_sectorTableSeeder.php

--- database/seeds/Ngg_sectorTableSeeder.php
+++ database/seeds/Ngg_sectorTableSeeder.php
@@ -12,148 +12,183 @@ class Ngg_sectorTableSeeder extends Seeder
     public function run()
     {
         DB::table('ngg_sector')->insert([
-            'nst_id' => '1',
+            
             'nst_name' => 'Management',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '2',
+            
             'nst_name' => 'IT',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '3',
+           
             'nst_name' => 'Business Consultant',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '4',
+            
             'nst_name' => 'Business Development',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '5',
+           
             'nst_name' => 'Sales',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '6',
+           
             'nst_name' => 'Marketing',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '7',
+            
             'nst_name' => 'HR',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '8',
+           
             'nst_name' => 'Customer Service',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '9',
+           
             'nst_name' => 'Sales Multi Brand',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '10',
+           
             'nst_name' => 'Graphic & Design',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '11',
+            
             'nst_name' => 'Business Planning& Development',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '12',
+           
             'nst_name' => 'Warehouse & Logistics',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '13',
+          
             'nst_name' => 'Procurement',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '14',
+            
             'nst_name' => 'Production',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '15',
+           
             'nst_name' => 'Finance & Accounting',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '16',
+            
             'nst_name' => 'Supply Chain',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '17',
+          
             'nst_name' => 'Quality Management',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '18',
+           
             'nst_name' => 'Diamond Today',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '19',
+          
             'nst_name' => 'Brand Development',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '20',
+           
             'nst_name' => 'Creative & Design',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '21',
+           
             'nst_name' => 'Sales & Marketing',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '22',
+           
             'nst_name' => 'Business & Merchandise Development',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '23',
+           
             'nst_name' => 'Provision & Lawyer',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
         DB::table('ngg_sector')->insert([
-            'nst_id' => '24',
+          
             'nst_name' => 'Human Resource',
             'nst_remark' => '',
             'nst_enable' => '1',
         ]);
+
+        DB::table('ngg_sector')->insert([
+            'nst_name' => 'Purchase',
+            'nst_remark' => '',
+            'nst_enable' => '1',
+        ]);
+
+        DB::table('ngg_sector')->insert([
+            'nst_name' => 'Gemologist',
+            'nst_remark' => '',
+            'nst_enable' => '1',
+        ]);
+        DB::table('ngg_sector')->insert([
+            'nst_name' => 'ผลิต',
+            'nst_remark' => '',
+            'nst_enable' => '1',
+        ]);
+        DB::table('ngg_sector')->insert([
+            'nst_name' => 'CAD Design',
+            'nst_remark' => '',
+            'nst_enable' => '1',
+        ]);
+        DB::table('ngg_sector')->insert([
+            'nst_name' => 'โรงงานผลิตน้ำแร่สวนผึ้ง',
+            'nst_remark' => '',
+            'nst_enable' => '1',
+        ]);
+        DB::table('ngg_sector')->insert([
+            'nst_name' => 'Sales Offline',
+            'nst_remark' => '',
+            'nst_enable' => '1',
+        ]);
+       
+
+       
     }
 }
